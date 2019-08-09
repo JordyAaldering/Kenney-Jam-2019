70ac73a0de5f9db725bb2177fbbc983e9223bbed
Jordy Aaldering <aalderingjordy@gmail.com>
2019-08-09 21:06:41 +0200

    rotate field

---
+++ Assets/Scripts/InputController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class InputController : MonoBehaviour
+{
+    private Rotator _rotator;
+
+    private void Awake()
+    {
+        _rotator = FindObjectOfType<Rotator>();
+    }
+
+    private void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        if (Mathf.Abs(horizontal) > 0f)
+        {
+            _rotator.Rotate(horizontal);
+        }
+    }
+}
---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    [SerializeField] private float _rotateSpeed = 1f;
+
+    public void Rotate(float direction)
+    {
+        transform.Rotate(0f, 0f, -direction * _rotateSpeed * Time.deltaTime);
+    }
+}
