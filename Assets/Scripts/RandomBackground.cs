d87532947ad466e304518a9a7330402026f58839
Jordy Aaldering <aalderingjordy@gmail.com>
2019-08-09 23:28:50 +0200

    background

---
+++ Assets/Scripts/RandomBackground.cs
@@ -0,0 +1,13 @@
+﻿#pragma warning disable 0649
+using Extensions;
+using UnityEngine;
+
+public class RandomBackground : MonoBehaviour
+{
+    [SerializeField] private Sprite[] _backgrounds;
+
+    private void Awake()
+    {
+        GetComponent<SpriteRenderer>().sprite = _backgrounds.GetRandom();
+    }
+}
